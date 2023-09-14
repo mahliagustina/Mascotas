02b2bc976c070e2eaea23f88ac3ecaf4f6609551
mahliagustina <mahliluz1@gmail.com>
2023-09-14 14:17:43 -0300

    Descripción de tus cambios

--- dos.js
+++ dos.js
@@ -2,9 +2,9 @@ let nombreIngresado = prompt("Ingresá tu nombre")
 
 let correoElectronico = prompt("Ingresá un mail de contacto")
 
-let animalEncontrado = prompt("Animal encontrado")
+let animalEncontrado = prompt("Animal perdido")
 
-let zonaEncontrado = prompt("Zona donde fue hallado")
+let zonaEncontrado = prompt("Zona donde se perdió")
 
 let colorPelo = prompt("Color de su pelaje")
 
@@ -13,5 +13,4 @@ let edadAproximada = prompt("Edad aproximada del animal")
 let ficha = `El usuario nuevo es ${nombreIngresado}, su mail de contacto es ${correoElectronico}, el animal encontrado es ${animalEncontrado}, la zona donde fue hallado es ${zonaEncontrado}, el color de su pelaje es ${colorPelo}, y la edad aproximada es ${edadAproximada}`
 
 alert (`Tu caso fue resgistrado con éxito. !Muchas gracias ${nombreIngresado}!`);
-
 console.log(ficha)
