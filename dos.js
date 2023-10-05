ef979dd55df9e6b8719cf5398c8dea5eb3e5ae34
mahliagustina <134453819+mahliagustina@users.noreply.github.com>
2023-10-05 15:35:59 -0300

    Update dos.js

--- dos.js
+++ dos.js
@@ -1,6 +1,8 @@
-let nombreIngresado = prompt("Ingresá tu nombre")
+let nombreIngresado = prompt("Ingresá tu nombre","");
+localStorage.setItem("nombreSesion", nombreIngresado);
 
-let correoElectronico = prompt("Ingresá un mail de contacto")
+let correoElectronico = prompt("Ingresá un mail de contacto");
+localStorage.setItem("mailSesion", correoElectronico);
 
 let animalPerdido = prompt("Animal perdido")
 
@@ -13,5 +15,6 @@ let edadAproximada = prompt("Edad aproximada del animal")
 let ficha = `El usuario nuevo es ${nombreIngresado}, su mail de contacto es ${correoElectronico}, el animal perdido es ${animalPerdido}, la zona donde se perdió es ${zonaPerdido}, el color de su pelaje es ${colorPelo}, y la edad aproximada es ${edadAproximada}`
 
 alert("Tu caso fue registrado con éxito. ¡Muchas gracias! " + nombreIngresado);
+sessionStorage.setItem("nombreSesion",ficha);
 
-console.log(ficha)
+// console.log(ficha); 
